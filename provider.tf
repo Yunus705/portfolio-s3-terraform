033e79d89b3a2ec4c4a4bad6dee534bc12222316
yunus <yunussharif705@gmail.com>
2025-09-27 16:05:05 +0530

    Updated provider.tf

--- provider.tf
+++ provider.tf
@@ -1,12 +1,3 @@
-# terraform {
-#   required_providers {
-#     aws = {
-#       source = "hashicorp/aws"
-#       version = "5.10.0"
-#     }
-#   }
-# }
-
 terraform {
   required_providers {
     aws = {
